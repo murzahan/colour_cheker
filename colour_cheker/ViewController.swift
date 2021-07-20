8e54846cc9c137355fe341db768a3d692295989c
murzahan <murzahan@gmail.com>
2021-07-20 21:13:56 +0500

    colour check final

--- colour_cheker/ViewController.swift
+++ colour_cheker/ViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var colorBox: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    
+    @IBOutlet weak var redLine: UISlider!
+    @IBOutlet weak var greenLine: UISlider!
+    @IBOutlet weak var blueLine: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorBox.layer.cornerRadius = 15
+        
+        redLine.minimumTrackTintColor = .red
+        greenLine.minimumTrackTintColor = .green
+        blueLine.minimumTrackTintColor = .blue
+        
+        colorBox.backgroundColor = UIColor(
+            red: CGFloat(redLine.value),
+            green: CGFloat(greenLine.value),
+            blue: CGFloat(blueLine.value),
+            alpha: 1
+            )
+    
+        redLabel.text = String(format: "%.2f", redLine.value)
+        greenLabel.text = String(format: "%.2f", greenLine.value)
+        blueLabel.text = String(format: "%.2f", blueLine.value)
+        
+        
+    
     }
 
-
+    @IBAction func rgbSlider(_ sender: UISlider) {
+    
+    
+    
+        colorBox.backgroundColor = UIColor(
+            red: CGFloat(redLine.value),
+            green: CGFloat(greenLine.value),
+            blue: CGFloat(blueLine.value),
+            alpha: 1
+        )
+        
+        redLabel.text = String(format: "%.2f", redLine.value)
+        greenLabel.text = String(format: "%.2f", greenLine.value)
+        blueLabel.text = String(format: "%.2f", blueLine.value)
+        
+    }
+    
+   
 }
+
 
